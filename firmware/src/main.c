6ec21b4239f27fee87192e2d16b472224002fb93
Sarah <72346043+sarahp31@users.noreply.github.com>
2022-09-08 10:01:56 -0300

    botao que para a musica

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -188,6 +188,22 @@ int divider = 0, noteDuration = 0;
 #define BUZZER_PIO_IDX			6
 #define BUZZER_PIO_IDX_MASK		(1 << BUZZER_PIO_IDX)
 
+
+
+/* flag */
+volatile char butchange_flag;
+volatile char butcontrol_flag;
+
+
+void butchange_callback(void){
+	
+	butchange_flag = 1;
+}
+
+void butcontrol_callback(void){
+	butcontrol_flag = !butcontrol_flag;
+}
+
 void init (void){
 	
 	board_init();
@@ -201,15 +217,36 @@ void init (void){
 	pmc_enable_periph_clk(BUTCONTROL_PIO_ID);
 	pmc_enable_periph_clk(BUZZER_PIO_ID);
 	
-	pio_set_input(BUTCHANGE_PIO, BUTCHANGE_PIO_IDX_MASK, PIO_DEFAULT);
-	pio_set_input(BUTCONTROL_PIO, BUTCONTROL_PIO_IDX_MASK, PIO_DEFAULT);
+	//pio_set_input(BUTCHANGE_PIO, BUTCHANGE_PIO_IDX_MASK, PIO_DEFAULT);
+	//pio_set_input(BUTCONTROL_PIO, BUTCONTROL_PIO_IDX_MASK, PIO_DEFAULT);
 	
 	pio_set_output(BUZZER_PIO, BUZZER_PIO_IDX_MASK, 0, 0, 0);
 	
-	pio_pull_up(BUTCHANGE_PIO, BUTCHANGE_PIO_IDX_MASK, 1);
-	pio_pull_up(BUTCONTROL_PIO,BUTCONTROL_PIO_IDX_MASK,1);
+	//pio_pull_up(BUTCHANGE_PIO, BUTCHANGE_PIO_IDX_MASK, 1);
+	//pio_pull_up(BUTCONTROL_PIO,BUTCONTROL_PIO_IDX_MASK,1);
 	
+	pio_configure(BUTCHANGE_PIO, PIO_INPUT, BUTCHANGE_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(BUTCHANGE_PIO, BUTCHANGE_PIO_IDX_MASK, 60);
+	pio_configure(BUTCONTROL_PIO, PIO_INPUT, BUTCONTROL_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(BUTCONTROL_PIO, BUTCONTROL_PIO_IDX_MASK, 60);
+	
+	pio_handler_set(BUTCHANGE_PIO, BUTCHANGE_PIO_ID, BUTCHANGE_PIO_IDX_MASK, PIO_IT_FALL_EDGE, butchange_callback);
+	pio_handler_set(BUTCONTROL_PIO, BUTCONTROL_PIO_ID, BUTCONTROL_PIO_IDX_MASK, PIO_IT_FALL_EDGE, butcontrol_callback);
+	
+	
+	pio_enable_interrupt(BUTCHANGE_PIO, BUTCHANGE_PIO_IDX_MASK);
+	pio_enable_interrupt(BUTCONTROL_PIO, BUTCONTROL_PIO_IDX_MASK);
+	pio_get_interrupt_status(BUTCHANGE_PIO);
+	pio_get_interrupt_status(BUTCONTROL_PIO);
+	
+	NVIC_EnableIRQ(BUTCHANGE_PIO_ID);
+	NVIC_EnableIRQ(BUTCONTROL_PIO_ID);
+	NVIC_SetPriority(BUTCHANGE_PIO_ID, 4);
+	NVIC_SetPriority(BUTCONTROL_PIO_ID, 4);
 }
+
+
+
 
 // Funções de controle
 
@@ -256,14 +293,17 @@ int get_selecao(void){
 void tone(int freq, int time){
 	
 	if(freq!=0){
-		double period = (1.0/freq)*1e6;    // perido em us
+		double period = (1.0/freq)*1e6;
 		double pulses = time*freq/1e3;
 	
 		for(int i = 0; i < pulses; i++){
-			set_buzzer();
-			delay_us(period/2);
-			clear_buzzer();
-			delay_us(period/2);
+			if(butcontrol_flag == 1){
+				break;
+			}
+				set_buzzer();
+				delay_us(period/2);
+				clear_buzzer();
+				delay_us(period/2);
 		}
 	} else {
 		set_buzzer();
